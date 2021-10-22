0561b2932c4800ee3c5fe88818a4cbe25f717cfe
Ivan Staev <IDStaev19@codingburgas.bg>
2021-10-22 21:17:58 +0300

    Add maze generating function

--- maze-game/maze-game/maze-game.cpp
+++ maze-game/maze-game/maze-game.cpp
@@ -86,9 +86,72 @@ void menu(int counter) {
 
 }
 
+void resetMaze(char* grid[], int width, int height, char wall) {
+    for (int i = 0; i < width; i++) {
+        for (int j = 0; j < height; j++) {
+            grid[i][j] = wall;
+        }
+    }
+}
+
+int isInPath(int x, int y, int width, int height) {
+    if (x >= 0 && x < width && y >= 0 && y < height) return true;
+    return false;
+}
+
+void visit(int x, int y, char* grid[], int width, int height, char wall) {
+    grid[x][y] = ' ';
+
+    int dirs[4] = { 0, 1, 2, 3 };
+
+    for (int i = 0; i < 4; ++i) {
+        int r = rand() & 3;
+        int temp = dirs[r];
+        dirs[r] = dirs[i];
+        dirs[i] = temp;
+    }
+
+    for (int i = 0; i < 4; ++i) {
+        int dx = 0, dy = 0;
+
+        switch (dirs[i]) {
+        case 0: dy = -1; break;
+        case 1: dx = 1; break;
+        case 2: dy = 1; break;
+        case 3: dx = -1; break;
+        }
+
+        int x2 = x + (dx * 2);
+        int y2 = y + (dy * 2);
+
+        if (isInPath(x2, y2, width, height)) {
+            if (grid[x2][y2] == wall) {
+                grid[x2 - dx][y2 - dy] = ' ';
+                visit(x2, y2, grid, width, height, wall);
+            }
+        }
+    }
+    grid[1][0] = ' ';
+    grid[width - 2][height - 1] = ' ';
+}
+
+void generateMaze(char* grid[], int width, int height, char wall) {
+    srand(time(0));
+    resetMaze(grid, width, height, wall);
+    visit(1, 1, grid, width, height, wall);
+}
+
 bool menuInput() {
     int choice;
     int counter = 1;
+
+    int height = 15, width = 15;
+
+    char wall = '#';
+
+    char** maze = new char* [height];
+    for (int i = 0; i < height; i++) maze[i] = new char[width];
+
     menu(counter);
     while (true)
     {
@@ -110,14 +173,24 @@ bool menuInput() {
         }// key down
         break;
         case ENTER: {
-            if (counter == 1)cout << "play"; 
+            if (counter == 1) {
+                system("cls");
+                generateMaze(maze, height, width, wall);
+                for (int i = 0; i < height; i++) {
+                    for (int j = 0; j < width; j++) {
+                        cout << maze[i][j] << " ";
+                    }
+                    cout << endl;
+                }
+                for (int i = 0; i < height; i++) {
+                    delete[] maze[i];
+                }
+            }
             if (counter == 2)cout << "rules";
             if (counter == 3)return 0;
         }
                   break;
         }// enter
-
-
     }
 }
 int main()
@@ -127,6 +200,3 @@ int main()
     } while (menuInput());
    
 }
-    
-      
-   
